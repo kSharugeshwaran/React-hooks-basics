e0143c17c1c24b7ba08fb16738519c2e7cfd9c1c
ksharugeshwaran <sharugeshwaran.k@kalvium.community>
2023-12-25 14:37:36 +0530

    done

---
+++ hooks/src/components/UseContext.jsx
@@ -0,0 +1,27 @@
+parentContext
+
+
+import React, { createContext, useState } from 'react'
+
+export const AppContext = createContext()
+
+const ParentContext = ({children}) => {
+
+  const [mainCount,setMainCount] = useState(50000)
+
+  const decreaseCount =()=>{
+    setMainCount(mainCount-1000)
+  }
+
+  const increaseCount = ()=>{
+    setMainCount(mainCount+1000)
+  }
+
+  const [isDark,setIsDark] = useState(true)
+
+  return <AppContext.Provider value={{mainCount,setMainCount,decreaseCount,increaseCount,isDark,setIsDark}}>
+          {children}
+  </AppContext.Provider>
+}
+
+export default ParentContext
